4936fe38d1cf3f7546cd461a68321a9fe7924081
gabrielgarcia2211 <61440596+gabrielgarcia2211@users.noreply.github.com>
2020-10-16 07:26:04 -0500

    cuarto commit

--- js/script.js
+++ js/script.js
@@ -9,6 +9,28 @@ function loadA() {
     xhttp.send();
 }
 
+function loadT() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("contenedor").innerHTML = this.responseText;
+      }
+    };
+    xhttp.open("GET", "tecnologias.html", true);
+    xhttp.send();
+}
+
+function loadF() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("contenedor").innerHTML = this.responseText;
+      }
+    };
+    xhttp.open("GET", "formulario.html", true);
+    xhttp.send();
+}
+
 
 
 function loadI() {
